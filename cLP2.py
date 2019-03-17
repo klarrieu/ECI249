6ee9ba40569925d7056f19ff281a5e5790e34ed4
Kenneth Gregory Larrieu <kglarrieu@ucdavis.edu>
2019-03-17 00:54:27 -0700

    finished first LP problem

--- cLP2.py
+++ cLP2.py
@@ -1,6 +1,5 @@
-import numpy as np
-import pandas as pd
 import pulp
+import pandas as pd
 
 
 def dot(l1, l2):
@@ -10,7 +9,7 @@ def dot(l1, l2):
 
 class LP:
     def __init__(self, *args):
-        self.q_names = ['< 5,000 cfs', '5-6,000 cfs', '6-8,000 cfs', '8-10,000 cfs', '10,000+ cfs']
+        self.q_names = ['<5,000 cfs', '5-6,000 cfs', '6-8,000 cfs', '8-10,000 cfs', '10,000+ cfs']
         self.q_probs = [0.8, 0.11, 0.06, 0.02, 0.01]
         if 'upstream' in args:
             self.q_probs = [0.9, 0.05, 0.03, 0.01, 0.01]
@@ -20,59 +19,75 @@ class LP:
         self.perm_costs = [10, 1, 40]
         self.perm_lims = [1e6, 200e3, 200e3]
         self.perm_reds = [[0, 100, 70, 60, 10], [0, 2, 3, 4, 7], [0, 100, 60, 50, 20]]
-        self.perm_reds = [[0, 200, 90, 70, 10], [0, 3, 4, 6, 10], [0, 100, 60, 50, 20]] # ***
+        # self.perm_reds = [[0, 200, 90, 70, 10], [0, 3, 4, 6, 10], [0, 100, 60, 50, 20]] # test case (Jay's paper example)
 
         self.em_names = ['Evacuate', 'Sandbagging', 'Heightened Levee Monitoring']
         self.em_costs = [200e3, 20e3, 1]
-        self.em_costs = [100e3, 30e3, 1] # ***
+        # self.em_costs = [100e3, 30e3, 1] # test case (Jay's paper example)
         self.em_lims = [1, 2, 20e3]
         self.em_reds = [[0, 200e3, 300e3, 500e3, 1e6], [0, 1e6, 800e3, 0, 0], [0, 2, 1, 0, 0]]
 
         # make set of linear coefficients for optimization
         # index 0-2: permanent options
-        self.coefficients = []
+
+        print('base damage: %.2f' % dot(self.q_probs, self.damage0))
+
+        self.cs = []
+        print('\nWorthwhile investments:\n')
         for i in range(len(self.perm_names)):
             c = self.perm_costs[i] - dot(self.q_probs, self.perm_reds[i])
-            self.coefficients.append(c)
+            if c < 0:
+                print(self.perm_names[i])
+            self.cs.append(c)
         # index 3-17: em options, listed primarily by q, secondarily by option
         for q in range(len(self.q_names)):
             for j in range(len(self.em_names)):
                 c = self.q_probs[q] * (self.em_costs[j] - self.em_reds[j][q])
-                self.coefficients.append(c)
+                if c < 0:
+                    print(self.em_names[j] + ', ' + self.q_names[q])
+                self.cs.append(c)
 
 
     def run_LP(self):
         # initialize decision variables
+        # list of permanent variables
         perm_vars = [pulp.LpVariable(self.perm_names[i], lowBound=0, upBound=self.perm_lims[i], cat='Integer') for i in range(len(self.perm_names))]
+        # list of dicts of emergency variables, dict for each emergency option has flow names as keys
         em_vars = [pulp.LpVariable.dict(self.em_names[j], self.q_names, lowBound=0, upBound=self.em_lims[j], cat='Integer') for j in range(len(self.em_names))]
 
-        # initialize model
+        self.varlist = perm_vars
+        for q in self.q_names:
+            for j in range(len(self.em_names)):
+                self.varlist.append(em_vars[j][q])
+
+        # initialize model, set to minimize objective fn
         model = pulp.LpProblem('Optimizing Floodplain Management', pulp.LpMinimize)
 
-        # initialize objective function
-        # costs of implementing permanent options
-        model.objective += pulp.lpSum([perm_vars[i] * self.perm_costs[i] for i in range(len(self.perm_names))])
-        # evc of implementing emergency options
-        for j, em_var in enumerate(em_vars):
-            model.objective += pulp.lpSum([self.q_probs[q] * self.em_costs[j] * em_var[self.q_names[q]] for q in range(len(self.q_names))])
-        # evc base damages
-        model.objective += pulp.lpSum([self.q_probs[q] * self.damage0[q] for q in range(len(self.q_names))])
-        # evc permanent damage reductions
-        for i, perm_var in enumerate(perm_vars):
-            model.objective -= pulp.lpSum([self.q_probs[q] * self.perm_reds[i][q] * perm_var for q in range(len(self.q_names))])
-        # evc emergency damage reductions
-        for j, em_var in enumerate(em_vars):
-            model.objective -= pulp.lpSum([self.q_probs[q] * self.em_reds[i][q] * em_var[self.q_names[q]] for q in range(len(self.q_names))])
+        # define objective function
+        model.objective += pulp.lpSum([self.cs[i] * self.varlist[i] for i in range(len(self.varlist))])
+        model.objective += dot(self.q_probs, self.damage0)
 
         # initialize constraints
-        for q in range(len(self.q_names)):
-            model += pulp.lpSum([self.q_probs[q] * self.perm_reds[i][q] * perm_vars[i] for i in range(len(self.perm_names))]) + pulp.lpSum([self.q_probs[q] * self.em_reds[j][q] * em_vars[j][self.q_names[q]] for j in range(len(self.em_names))]) <= self.damage0[q]
+        for q in range(1, len(self.q_names)):
+            model += pulp.lpSum([self.perm_reds[i][q] * perm_vars[i] for i in range(len(self.perm_names))]) + pulp.lpSum([self.em_reds[j][q] * em_vars[j][self.q_names[q]] for j in range(len(self.em_names))]) <= self.damage0[q]
 
         model.solve()
-        print(pulp.value(model.objective))
-        print(model.)
+        print('\nModel status: %s\n' % pulp.LpStatus[model.status])
+        print('Model Objective:')
+        print(model.objective)
+        print('\nModel constraints:')
+        print(model.constraints)
+        print('\nObjective function value: %.2f' % pulp.value(model.objective))
+        print('\nOptimal values:\n')
+        for var in model.variables():
+            print var
+            print var.varValue
 
+        perm_df = pd.DataFrame({'Implemented Quantity': [perm_vars[i].value() for i in range(len(self.perm_names))]}, index=self.perm_names)
+        
+        print(perm_df)
         return model
+
 
 
 lp = LP()
